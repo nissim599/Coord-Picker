e5e1230f80d35823898bdc9230dcb76414d48a2e
CoreForge <fortnitenissim@gmail.com>
2026-02-14 22:43:07 +0200

    Create fxmanifest.lua for rl_coord_picker
    Add fxmanifest for rl_coord_picker with metadata and client script.

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'rl_coord_picker'
+author 'By Nissim'
+version '1.1.0'
+description 'In-game coordinate picker: /pickcoords then click an entity to print vector4 coords (with safe notifications).'
+
+client_scripts {
+  'client/main.lua'
+}
